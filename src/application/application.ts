f53d8650ee50b159ec9f581d4d23f08003cc0efe
Alex <olivesargreek@gmail.com>
2021-12-14 10:11:06 +0300

    feat: control.ts & signal.ts & state.ts

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,7 +1,10 @@
 import Control from "../common/control";
+import { SettingsPage } from './settingsPage';
+import { StartPage } from './startPage';
 
 export class Application extends Control {
   constructor(parentNode: HTMLElement) {
     super(parentNode);
+    const startPage = new StartPage(this.node);
   }
 }
--- src/application/startPage.ts
+++ src/application/startPage.ts
@@ -3,5 +3,6 @@ import Control from "../common/control";
 export class StartPage extends Control {
   constructor(parentNode: HTMLElement) {
     super(parentNode);
+    const settingsButton = new Control(this.node, 'button', '', 'settings');
   }
 }
