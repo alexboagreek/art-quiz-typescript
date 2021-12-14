3a9c5ec177a138f2a888f5f43ccac37fa676b338
Alex <olivesargreek@gmail.com>
2021-12-14 12:54:59 +0300

    feat: import CategoriesPage from './categories

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,6 +1,7 @@
 import Control from "../common/control";
 import { SettingsPage } from './settingsPage';
 import { StartPage } from './startPage';
+import { CategoriesPage } from './categoriesPage';
 
 export class Application extends Control {
   constructor(parentNode: HTMLElement) {
@@ -10,6 +11,10 @@ export class Application extends Control {
   }
   private mainCycle() {
     const startPage = new StartPage(this.node);
+    startPage.onGameSelect = () => {
+      const categories = new CategoriesPage(this.node);
+    }
+
     startPage.onSettings = () => {
       startPage.destroy();
       const settingsPage = new SettingsPage(this.node);
