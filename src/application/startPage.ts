3132079f355d9bb1e02221ce830ab4c98dc2cd64
Alex <olivesargreek@gmail.com>
2021-12-14 12:14:06 +0300

    create: artistsButton & picturesButton

--- src/application/startPage.ts
+++ src/application/startPage.ts
@@ -2,8 +2,22 @@ import Control from "../common/control";
 
 export class StartPage extends Control {
   onSettings: () => void;
+  onGameSelect: (gameName: string) => void;
+
+
+
+
+
   constructor(parentNode: HTMLElement) {
     super(parentNode);
+    // create picturesButton
+    const picturesButton = new Control(this.node, 'button', '', 'pictures');
+    picturesButton.node.onclick = () => this.onGameSelect('pictures');
+
+    // create artistsButton
+    const artistsButton = new Control(this.node, 'button', '', 'artists');
+    artistsButton.node.onclick = () => this.onGameSelect('artists');
+
     const settingsButton = new Control(this.node, 'button', '', 'settings');
     settingsButton.node.onclick = () => this.onSettings();
   }
