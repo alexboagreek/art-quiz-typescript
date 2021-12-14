e7308de7066afa83fa72ab20c63831e8559d36ba
Alex <olivesargreek@gmail.com>
2021-12-14 17:40:35 +0300

    start: question cycle agregat

---
+++ src/application/artistsQuestionView.ts
@@ -0,0 +1,24 @@
+
+import Control from "../common/control";
+interface IArtistsQuestionData {
+  answers: Array<any>
+
+}
+
+export class GameFieldPage extends Control {
+    onAnswer: (index:number)=> void;
+  
+    constructor(parentNode: HTMLElement, questionData: IArtistsQuestionData) {
+      super(parentNode);
+      
+      const question = new Control(this.node, 'div', '', 'Вопрос?');
+      const answerButtons = questionData.answers.map((it, i) => {
+        const button = new Control(this.node, 'button', '', i.toString());
+        button.node.onclick = () => {
+          this.onAnswer(i);
+        }
+
+      });
+      
+    } 
+} 
